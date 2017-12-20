1c3ad225944e6d3b8e8a141b7b7f00a8dfa11967
romec512 <roma-asadov@mail.ru>
2017-12-21 02:59:05 +0300

    лабу не доделал, но сделана основная логика, описан контейнерный класс с добавлением, показом и перемещением объектов, осталось доделать удаление контейнера, стирание элементов, добавить контейнер на основе списка.

---
+++ TestWPF/TestWPF/Containers.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Containers.xaml
+    /// </summary>
+    public partial class Containers : Window
+    {
+        private MassContainer container;
+        public Containers()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, RoutedEventArgs e)
+        {
+            container = new MassContainer();
+        }
+
+        private void buttonShow_Click(object sender, RoutedEventArgs e)
+        {
+            if (container.Count != 0)
+            {
+                container.Show(canvas);
+            }
+            else
+            {
+                MessageBox.Show("Вы не заполнили массив!");
+            }
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            canvas.Children.Clear();
+            switch(rand.Next(4))
+            {
+                case 0:
+                    container.Add(new Circle(rand.Next(800), rand.Next(400), rand.Next(200)));
+                    container.Show(canvas);
+                    break;
+                case 1:
+                    container.Add(new Rectangles(rand.Next(700), rand.Next(400), rand.Next(300), rand.Next(200)));
+                    container.Show(canvas);
+                    break;
+                case 2:
+                    container.Add(new Ring(rand.Next(700), rand.Next(400), rand.Next(200), rand.Next(300)));
+                    container.Show(canvas);
+                    break;
+                case 3:
+                    container.Add(new Rhombus(rand.Next(700), rand.Next(400), rand.Next(300), rand.Next(200)));
+                    container.Show(canvas);
+                    break;
+            }
+        }
+
+        private void buttonMove_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+            container.Move(canvas);
+        }
+    }
+}
---
+++ TestWPF/TestWPF/MassContainer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TestWPF
+{
+    class MassContainer
+    {
+        private ArrayList figures;
+        public int Count { get { return _count; } }
+        private int _count;
+        public MassContainer()
+        {
+            figures = new ArrayList();
+            _count = 0;
+        }
+
+        private void Iterator(string str,Canvas canvas = null, TextBox textBoxX = null, TextBox textBoxY = null)
+        {
+            foreach(TFigure fig in figures)
+            {
+                switch(str)
+                {
+                    case "show":
+                        fig.Show(canvas);
+                        break;
+                    case "move":
+                        fig.Move(textBoxX, textBoxY, canvas);
+                        break;
+                    case "delete":
+                        figures.Clear();
+                        MessageBox.Show("Вы удалили все объекты");
+                        break;
+                }
+            }
+        }
+        public void Add(TFigure figure)
+        {
+            figures.Add(figure);
+            _count++;
+        }
+
+        public void Show(Canvas canvas)
+        {
+            this.Iterator("show",canvas);
+        }
+        public void Move(Canvas canvas)
+        {
+            TextBox textBoxX = new TextBox();
+            TextBox textBoxY = new TextBox();
+            Random rand = new Random();
+            textBoxX.Text = Convert.ToString(rand.Next(700));
+            textBoxY.Text = Convert.ToString(rand.Next(400));
+            this.Iterator("move",canvas, textBoxX, textBoxY);
+        }
+        public void Delete(TFigure figure)
+        {
+            this.Iterator("delete");
+        }
+    }
+}
